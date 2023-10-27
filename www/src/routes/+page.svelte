b9d35b12546bd52d02ae6417f341d841bbd37c11
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-27 19:13:56 +0200

    created main page layout

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+
+<div>
+    <h1 id="title"> Pomme </h1>
+    
+    <div id="search">
+        <h3 id="description"> Cherchez un film ou une série pour savoir si elle est safe ! </h3>
+        <input type="text" id="search-bar"/>
+    </div>
+
+    <footer style="bottom: 1%; position: absolute; left:0; width: 100%; margin: 0; overflow: hidden;">
+        <div style="text-align: center;"> Made with 🍑 by <a href="https://github.com/Olyxz16"> Cédric Sazos </a> </div>
+    </footer>
+
+</div>
+
+
+
+
+<style>
+
+    :global(body) {
+        background-color: rgb(82, 76, 146);
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+    }
+
+    #title {
+        position: absolute;
+        top: 5%;
+        left: 50%;
+        transform: translate(-50%, -5%);
+        -webkit-transform: translate(-50%, -5%);
+    }
+
+    #search {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        -webkit-transform: translate(-50%, -50%);
+    }
+
+    #search-bar {
+        position: relative;
+        margin-top: 15px;
+        left: 50%;
+        transform: translateX(-50%);
+        -webkit-transform: translateX(-50%);
+
+        width: 130%;
+        font-size: 2em;
+
+        border-radius: 25px;
+    }
+
+
+
+</style>
