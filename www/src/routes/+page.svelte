08ea96da75d512be140e8b5b26fc653d91fb3934
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-28 18:54:23 +0200

    updated main page

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -1,13 +1,15 @@
 
-
-
-
 <div>
     <h1 id="title"> Pomme </h1>
     
     <div id="search">
         <h3 id="description"> Cherchez un film ou une série pour savoir si elle est safe ! </h3>
-        <input type="text" id="search-bar"/>
+        <form method="GET" action="/search">
+            <div id="search-bar-div">
+                <input type="text" id="search-bar" name="title"/>
+                <input type="submit" id="search-button">
+            </div>
+        </form>
     </div>
 </div>
 
@@ -32,17 +34,30 @@
         -webkit-transform: translate(-50%, -50%);
     }
 
-    #search-bar {
+    #search-bar-div {
         position: relative;
         margin-top: 15px;
         left: 50%;
         transform: translateX(-50%);
         -webkit-transform: translateX(-50%);
 
+        display: flex;
+        justify-content: space-between;
+    }
+
+    #search-bar {
         width: 130%;
         font-size: 2em;
-
         border-radius: 25px;
+    }
+
+    #search-button {
+        width: 50px;
+        min-width: 50px;
+        height: 50px;
+        min-height: 50px;
+        border-radius: 25px;
+        margin-left: 10px;
     }
 
 
