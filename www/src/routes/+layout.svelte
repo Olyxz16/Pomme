fc9f18e30122100774c9a22e3adbed13109909dc
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-30 21:02:08 +0100

    added alpha mention and changed background color

--- www/src/routes/+layout.svelte
+++ www/src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <footer style="bottom: 1%; position: absolute; left:0; width: 100%; margin: 0; overflow: hidden;">
-    <div style="text-align: center;"> Made with 🍑 by <a href="https://github.com/Olyxz16"> Cédric Sazos </a> </div>
+    <div style="text-align: center;"> Alpha version | Made with 🍑 by <a href="https://github.com/Olyxz16"> Cédric Sazos </a> </div>
 </footer>
 
 
 <style> 
 
     :global(body) {
-        background-color: rgb(82, 76, 146);
+        background-color: rgb(108, 101, 184);
         display: flex;
         flex-direction: column;
         text-align: center;
