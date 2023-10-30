5811f9fc7b825ad7c063921a536daf825160862c
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-30 15:19:00 +0100

    Put SearchArea in its own file

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -1,21 +1,14 @@
+<script>
+  import SearchArea from "./SearchArea.svelte";
+
+</script>
 
 <div>
     <h1 id="title"> Pomme </h1>
     
     <div id="search">
-        <h3 id="description"> Cherchez un film ou une série pour savoir si elle est safe ! </h3>
-        <form method="GET" action="/search">
-            <div id="search-bar-div">
-                <input type="text" id="search-bar" name="title"/>
-                <input type="submit" id="search-button">
-            </div>
-            <div id="numbers-div">
-                <label for="season"> Saison </label>
-                <input type="number" name="season"/>
-                <label for="episode"> Episode </label>
-                <input type="number" name="episode"/>
-            </div>
-        </form>
+        <SearchArea />
+        <hr>
     </div>
 </div>
 
@@ -40,37 +33,8 @@
         -webkit-transform: translate(-50%, -50%);
     }
 
-    #search-bar-div {
-        position: relative;
-        margin-top: 15px;
-        left: 50%;
-        transform: translateX(-50%);
-        -webkit-transform: translateX(-50%);
-
-        display: flex;
-        justify-content: space-between;
+    hr {
+        transform: translateY(50px);
     }
-
-    #search-bar {
-        width: 130%;
-        font-size: 2em;
-        border-radius: 25px;
-    }
-
-    #search-button {
-        width: 50px;
-        min-width: 50px;
-        height: 50px;
-        min-height: 50px;
-        border-radius: 25px;
-        margin-left: 10px;
-    }
-
-
-    #numbers-div {
-        margin-top: 30px;
-    }
-
-
 
 </style>
---
+++ www/src/routes/SearchArea.svelte
@@ -0,0 +1,52 @@
+
+
+
+<h3 id="description"> Cherchez un film ou une série pour savoir si elle est safe ! </h3>
+<form method="GET" action="/search">
+    <div id="search-bar-div">
+        <input type="text" id="search-bar" name="title"/>
+        <input type="submit" value="Vérifier" id="search-button">
+    </div>
+    <div id="numbers-div">
+        <label for="season"> Saison </label>
+        <input type="number" name="season"/>
+        <label for="episode"> Episode </label>
+        <input type="number" name="episode"/>
+    </div>
+</form>
+
+
+
+<style>
+    #search-bar-div {
+        position: relative;
+        margin-top: 15px;
+        left: 50%;
+        transform: translateX(-50%);
+        -webkit-transform: translateX(-50%);
+
+        display: flex;
+        justify-content: space-between;
+    }
+
+    #search-bar {
+        width: 130%;
+        font-size: 2em;
+        border-radius: 25px;
+    }
+
+    #search-button {
+        width: 50px;
+        min-width: 50px;
+        height: 50px;
+        min-height: 50px;
+        border-radius: 25px;
+        margin-left: 10px;
+    }
+
+
+    #numbers-div {
+        margin-top: 30px;
+    }
+
+</style>
