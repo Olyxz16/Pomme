75533ebf748d24a79b5af42153327ec8d544b116
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-31 10:46:01 +0100

    Added GET handler to +server api

--- www/src/routes/api/+server.ts
+++ www/src/routes/api/+server.ts
@@ -1,8 +1,10 @@
-import { setSafe, setUnsafe } from '$lib/db';
+import { getEpisode, getSingle, setSafe, setUnsafe } from '$lib/db';
 import { toTimestamp } from '$lib/timestamps';
+import { error } from '@sveltejs/kit';
 
 
 export async function POST({ request }) : Promise<any> {
+
 	const data = await request.json();
 	
 	if(data.safe) {
@@ -16,6 +18,19 @@ export async function POST({ request }) : Promise<any> {
 }
 
 
-export async function GET({ request }) : Promise<any> {
-	
+export async function GET({ url }) : Promise<any> {
+
+	let title = url.searchParams.get("title");
+    let season = Number.parseInt(url.searchParams.get("season") ?? "0");
+    let ep = Number.parseInt(url.searchParams.get("ep") ?? "0");
+
+	if(title === undefined || title === null) {
+        throw error(404);
+    }
+
+    if(season === undefined && ep === undefined) {
+        return await getSingle(title);
+    }
+    const data = await getEpisode(title, season, ep);
+	return new Response(JSON.stringify(data));
 }
