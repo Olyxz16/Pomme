02398b58d94188a783b4b77c56d1f17b8f099d6d
Cédric Sazos <cedric.sazos@tutanota.com>
2023-11-04 18:12:25 +0100

    Added title safety in search

--- www/src/routes/search/+page.server.ts
+++ www/src/routes/search/+page.server.ts
@@ -1,5 +1,6 @@
 import { error } from '@sveltejs/kit';
 import { getSingle, getEpisode } from '$lib/db';
+import { query } from '$lib/moviedb.js';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ url }) {
@@ -9,6 +10,11 @@ export async function load({ url }) {
     let ep = Number.parseInt(url.searchParams.get("ep") ?? "0");
 
     if(title === undefined || title === null) {
+        throw error(404);
+    }
+
+    const values = await query(title);
+    if(!values.includes(title)) {
         throw error(404);
     }
 
