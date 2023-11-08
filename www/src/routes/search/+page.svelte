cb9f43f600b034cfce73f3c1472d95ab5177d16f
Cédric Sazos <cedric.sazos@tutanota.com>
2023-11-08 12:32:47 +0100

    Renamed Episode to MediaResult

--- www/src/routes/search/+page.svelte
+++ www/src/routes/search/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 
-  import Episode from './Episode.svelte';
+  import MediaResult from './MediaResult.svelte';
   import UpdateArea from '../UpdateArea.svelte';
 
     /** @type {import('./$types').PageData} */
@@ -15,7 +15,7 @@
 </svelte:head>
 <div> 
     <div id="episode">
-      <Episode data={data} />
+      <MediaResult data={data} />
     </div>
     <div id="update">
       <UpdateArea title={data.title} season={data.season} ep={data.ep}/>
