26f140decbef1338901e3773fa9e0f63cced19ab
Cédric Sazos <cedric.sazos@tutanota.com>
2023-11-13 15:27:38 +0100

    Added movie rest api

---
+++ www/src/routes/api/rest/movies/+server.ts
@@ -0,0 +1,23 @@
+import { error } from "@sveltejs/kit";
+import { query } from '$lib/moviedb';
+
+
+export async function GET({ url }) : Promise<any> {
+
+    console.log("test");
+
+	let title = url.searchParams.get("title");
+
+	if(title === undefined || title === null) {
+        throw error(404);
+    }
+
+    const data = await query(title);
+    let result = "";
+    let maxInd = Math.min(5, data.length);
+    for(let i = 0 ; i < maxInd ; i++) {
+        result += `<option value=${data[i]}></option>` + "\n";
+    }
+    return new Response(result);
+
+}
