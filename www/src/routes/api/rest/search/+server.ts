73a5294e76634d79be0820aa9b0448f687fbafe3
Cédric Sazos <cedric.sazos@tutanota.com>
2023-11-22 16:24:00 +0100

    Added search api support for extension

--- www/src/routes/api/rest/search/+server.ts
+++ www/src/routes/api/rest/search/+server.ts
@@ -1,15 +1,36 @@
-import { error } from "@sveltejs/kit";
-import { query } from '$lib/moviedb';
+import { queryTitle } from '$lib/query';
+import { fromTimestamp } from '$lib/timestamps';
 
 
 export async function GET({ url }) : Promise<any> {
-
-	let title = url.searchParams.get("title");
-
-	if(title === undefined || title === null) {
-        throw error(404);
+    const data = await queryTitle(url);
+    const title: string = data.title;
+    const season: number = data.season;
+    const ep: number = data.ep;
+    const safe: boolean = data.safe;
+    const timestamps: number[] = data.timestamps;
+    console.log(timestamps);
+    let res = `
+        <p> ${title} </p>
+        <p> ${season} </p>
+        <p> ${ep} </p> 
+    `;
+    if(safe) {
+        res += `<p> Est safe ! </p>`;
+    } else if(timestamps === undefined || timestamps.length === 0) {
+        res += `<p> Pas de données. </p>`;
+    } else {
+        res += `<p> N'est pas safe ! </p>
+                <p> Aux timestamps : </p>`;
+        timestamps?.forEach((ts: number) => {
+            let { hour, minute, second} = fromTimestamp(ts);
+            res += `
+                <p> ${hour} </p>
+                <p> ${minute} </p>
+                <p> ${second} </p>
+            `;
+        });
     }
-
-    const data = await query(title);
-	return new Response(`<p> ${title} </p>`);
+    
+	return new Response(res);
 }
