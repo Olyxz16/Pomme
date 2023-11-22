a02fc8521bf69a7f5750d649b914255527588b2d
Cédric Sazos <cedric.sazos@tutanota.com>
2023-11-22 17:34:36 +0100

    Added new hook rule

--- www/src/hooks.server.ts
+++ www/src/hooks.server.ts
@@ -2,12 +2,24 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ resolve, event }) => {
   // Apply CORS header for API routes
-  if (event.url.pathname.startsWith('/api/rest')) {
+  if (event.url.pathname.startsWith('/api/rest') ) {
     // Required for CORS to work
     if(event.request.method === 'OPTIONS') {
       return new Response(null, {
         headers: {
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+          'Access-Control-Allow-Methods': 'GET',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': '*',
+        }
+      });
+    }
+  }
+  if (event.url.pathname.startsWith('/add') ) {
+    // Required for CORS to work
+    if(event.request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Methods': 'POST',
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Headers': '*',
         }
@@ -17,7 +29,10 @@ export const handle: Handle = async ({ resolve, event }) => {
 
   const response = await resolve(event);
   if (event.url.pathname.startsWith('/api/rest')) {
-        response.headers.append('Access-Control-Allow-Origin', `*`);
+    response.headers.append('Access-Control-Allow-Origin', `*`);
+  }
+  if (event.url.pathname.startsWith('/add')) {
+    response.headers.append('Access-Control-Allow-Origin', `*`);
   }
   return response;
 };
